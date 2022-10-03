de64ae6277e303637feb44969d7381b11f5821f7
Arjun Raja Yogidas <yogidas.a@northeastern.edu>
2022-10-03 09:09:40 -0400

    added auth guard initial logic

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,23 @@
-import { Controller, Get, Post, Res, HttpStatus } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthGuard } from './auth/auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor() {}
+
+  @Get()
+  initial() {
+    return {status : "200 OK - hello world"};
+  }
 
   @Get('/healthz')
   healthz() {
     return {status : "200 OK"};
   }
 
-  // @Get('/external')
-  // async external(): Promise<any> {
-  //   return await this.appService.external();
-  // } 
+  @UseGuards(AuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return req.user;
+  }
 }
