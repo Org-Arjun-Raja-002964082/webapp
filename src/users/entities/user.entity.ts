3e682da96b13416dd215301168a0ffca60c977c4
Arjun Raja Yogidas <yogidas.a@northeastern.edu>
2022-10-03 23:14:05 -0400

    added postgres datbase connection and env file, user entitiy

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,51 @@
-export class User {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn({
+        type: 'bigint',
+        name: 'user_id',
+    })
+    id: number;
+
+    @Column({
+        name: 'email_address',
+        nullable: false,
+        default: '',
+    })
+    email: string;
+
+    @Column({
+        name: 'password',
+        nullable: false,
+        default: '',
+    })
+    password: string;
+
+    @Column({
+        name: 'first_name',
+        nullable: false,
+        default: '',
+    })
+    firstName: string;
+
+    @Column({
+        name: 'last_name',
+        nullable: false,
+        default: '',
+    })
+    lastName: string;
+
+    @CreateDateColumn({ 
+        type: "timestamp", 
+        default: () => "CURRENT_TIMESTAMP(6)" 
+    })
+    created_at: Date;
+
+    @UpdateDateColumn({ 
+        type: "timestamp", 
+        default: () => "CURRENT_TIMESTAMP(6)", onUpdate: "CURRENT_TIMESTAMP(6)" 
+    })
+    updated_at: Date;
+
+}
