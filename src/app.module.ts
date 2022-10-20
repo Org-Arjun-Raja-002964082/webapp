f240c5b6243e6940f531ab34afc16d8324160d36
Arjun Raja Yogidas <yogidas.a@northeastern.edu>
2022-10-20 19:41:00 -0400

    minor fix: error with env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,11 +23,11 @@ import entities from '../typeorm_entities';
     imports: [ConfigModule],
     useFactory: async (configService: ConfigService) => ({
       type: 'postgres',
-      host: configService.get('DB_HOST'),
-      port: configService.get('DB_PORT'),
-      username: configService.get('DB_USERNAME'),
-      password: configService.get('DB_PASSWORD'),
-      database: configService.get('DB_NAME'),
+      host:  'localhost',
+      port:   5432,
+      username: 'postgres',
+      password: 'admin',
+      database: 'webapp_db',
       entities: entities,
       synchronize: true, // remove this in production
     }),
