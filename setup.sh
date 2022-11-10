dab7466338207f89dac9481956c8b719df9c4189
Arjun Raja Yogidas <yogidas.a@northeastern.edu>
2022-11-10 12:24:14 -0500

    update node installation

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get clean
-curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
-sudo apt-get install nodejs -y
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - &&\
+sudo apt-get install -y nodejs
 sudo chown ubuntu ./*
 sudo apt-get install zip unzip
 echo "=========================================== Pre shell commands ==========================================="
